672a724e246c1fbb361b9168616510f28c4752b5
ZHANG Ning <zhn038@gmail.com>
2021-12-08 13:48:25 +0800

    Update get_emnews.py

--- get_emnews.py
+++ get_emnews.py
@@ -25,5 +25,5 @@ categories: emnews
 tags: 东财滚动新闻
 ---
 
-"""+i['digest'].split("】")[-1]+"\n\n<"+i['url_w']+">\n\n[返回东财滚动新闻](./emnews/)\n[返回首页](./)")
+"""+i['digest'].split("】")[-1]+"\n\n<"+i['url_w']+">\n\n[返回东财滚动新闻](../emnews/)\n[返回首页](../)")
     # print(result)
