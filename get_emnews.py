3cb524825ac357dba5f3d6523c7f85191409e415
ZHANG Ning <zhn038@gmail.com>
2021-12-14 15:13:05 +0800

    Update get_emnews.py

--- get_emnews.py
+++ get_emnews.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     result = get_url("http://183.136.162.242/web/webapi?type=102&count=500&newsid=1")
     result = json.loads(result)['records']
     for i in result:
+        contents = json.loads(get_url("http://choicewzp1.eastmoney.com/NewsData/GetNewsText.do?id="+i['infoCode']))['content']
         # print(i['title'], i['digest'].split("】")[-1], i['id'], i['url_w'], i['showtime'], i['showtime'].split(" ")[0])
         with open("./_posts/"+str(i['showtime'].split(" ")[0])+"-"+str(i['id'])+".md", "w", encoding="utf-8") as f:
             f.write("""---
@@ -24,6 +25,5 @@ date: """+i['showtime']+""" +0800
 categories: emnews
 tags: 东财滚动新闻
 ---
-
-"""+i['digest'].split("】")[-1]+"\n\n<"+i['url_w']+">\n\n[返回东财滚动新闻](//finews.withounder.com/emnews/)｜[返回首页](//finews.withounder.com/)")
+> """+i['digest'].split("】")[-1]+"\n\n"+contents+"\n\n<"+i['url_w']+">\n\n[返回东财滚动新闻](//finews.withounder.com/emnews/)｜[返回首页](//finews.withounder.com/)")
     # print(result)
