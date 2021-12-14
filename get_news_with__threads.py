1d98961b921661aa2a0857ee97ea008ffae3b90f
ZHANG Ning <zhn038@gmail.com>
2021-12-14 21:24:26 +0800

    Update get_news_with__threads.py

--- get_news_with__threads.py
+++ get_news_with__threads.py
@@ -70,23 +70,15 @@ tags: """+i['medianame']+"""新闻
 """+contents.replace("　　","")+"\n\n<http://choicewzp1.eastmoney.com/html_News/NewsShare.html?infoCode="+i['infoCode']+">\n\n[返回"+i['medianame']+"新闻](//finews.withounder.com/category/"+pinyin+".html)｜[返回首页](//finews.withounder.com/)")
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=get_esnews, name='get_esnews')
-    t1.start()
-    t2 = threading.Thread(target=get_esnews_2, name='get_esnews_2')
-    t2.start()
-    t3 = threading.Thread(target=get_market_news, name='get_market_news', args=(["37"],))
-    t3.start()
-    t4 = threading.Thread(target=get_market_news, name='get_market_news', args=(["38"],))
-    t4.start()
-    t5 = threading.Thread(target=get_market_news, name='get_market_news', args=(["39"],))
-    t5.start()
-    # 创建两个线程
-#     try:
-#         _thread.start_new_thread( get_esnews )
-#         # _thread.start_new_thread( get_esnews_2 )
-#         # _thread.start_new_thread( get_market_news, () )
-#         # _thread.start_new_thread( get_market_news, (["38"]) )
-#         # _thread.start_new_thread( get_market_news, (["39"]) )
-#     except:
-#         print("Error: unable to start thread")
-#  
+    for i in range(0,3):
+        t1 = threading.Thread(target=get_esnews, name='get_esnews')
+        t1.start()
+        t2 = threading.Thread(target=get_esnews_2, name='get_esnews_2')
+        t2.start()
+        t3 = threading.Thread(target=get_market_news, name='get_market_news', args=(["37"],))
+        t3.start()
+        t4 = threading.Thread(target=get_market_news, name='get_market_news', args=(["38"],))
+        t4.start()
+        t5 = threading.Thread(target=get_market_news, name='get_market_news', args=(["39"],))
+        t5.start()
+        time.sleep(30)
